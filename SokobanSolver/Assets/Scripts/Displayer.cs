cdfdc66a28aff69cd5484a2c710ba96d41907df8
HBence0408 <61012730+HBence0408@users.noreply.github.com>
2025-05-20 10:35:05 +0200

    Displayer

---
+++ SokobanSolver/Assets/Scripts/Displayer.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Displayer : MonoBehaviour
+{
+
+    private static Displayer instance;
+    public static Displayer Instance { get => instance; }
+
+    [SerializeField] private Transform man;
+    [SerializeField] private Transform[] balls = new Transform[3];
+    [SerializeField] private GameObject wallPrefab;
+    [SerializeField] private GameObject goalPrefab;
+    Stack<State> solution;
+    private bool solve = true;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Debug.LogWarning("mutiple dispalyer singletons");
+        }
+    }
+
+    
+    private void Start()
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                if (State.Map[i,j] == '#')
+                {
+                    Instantiate(wallPrefab, new Vector3(j, i, 0), this.transform.rotation, null);
+                }
+                if (State.Map[i, j] == '1')
+                {
+                    Instantiate(goalPrefab, new Vector3(j, i, 0), this.transform.rotation, null);
+                }
+            }
+        }
+
+        
+    }
+
+    private void Update()
+    {
+
+        if (solve)
+        {
+            (int, int)[] ballsStartPos = { (2, 2), (2, 3), (3, 2) };
+            (int, int) manStartPos = (1, 1);
+            Backtrack backtrack = new Backtrack(100, new Node(new State(manStartPos, ballsStartPos)));
+
+            Node terminalNode = backtrack.FindTerminalNode();
+
+            solution = backtrack.Solution(terminalNode);
+            solve = false;
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            State s = solution.Pop();
+
+            man.position = new Vector3(s.Man.Item2, s.Man.Item1, 0);
+            for (int i = 0; i < 3; i++)
+            {
+                balls[i].position = new Vector3(s.Balls[i].Item2, s.Balls[i].Item1, 0);
+            }
+        }
+    }
+}
